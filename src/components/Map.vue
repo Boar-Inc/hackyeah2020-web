d512c66dfa33057cf5cfb31aefea4d1ec6e652e6
Jakub Kopańko <jakub@kopanko.com>
2020-11-27 00:48:33 +0100

    heatmap and clustering

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,28 +2,21 @@
   <div id="map-wrap" style="height: 100vh">
     <client-only>
       <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true">
-        <vl-view v-if="center" :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation" />
+        <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation" />
         <vl-layer-tile v-if="center">
           <vl-source-osm />
         </vl-layer-tile>
 
-        <!-- <vl-layer-vector v-if="useUrlFunction">
-          <vl-source-vector :url="urlFunction" :strategy-factory="loadingStrategyFactory" />
-        </vl-layer-vector>
-        <vl-layer-vector v-else>
-          <vl-source-vector url="https://openlayers.org/en/latest/examples/data/geojson/countries.geojson" projection="EPSG:4326" />
-        </vl-layer-vector> -->
+        <vl-layer-heatmap :radius="10">
+          <vl-source-vector :features.sync="geo" />
+        </vl-layer-heatmap>
 
-        <vl-geoloc @update:position="center = $event">
-          <template slot-scope="geoloc">
-            <vl-feature v-if="geoloc.position" id="position-feature">
-              <vl-geom-point :coordinates="geoloc.position" />
-              <vl-style-box>
-                <vl-style-icon src="_media/marker.png" :scale="0.4" :anchor="[0.5, 1]" />
-              </vl-style-box>
-            </vl-feature>
-          </template>
-        </vl-geoloc>
+        <vl-layer-vector>
+          <vl-source-cluster>
+            <vl-source-vector :features.sync="geo" />
+          </vl-source-cluster>
+        </vl-layer-vector>
+
       </vl-map>
     </client-only>
   </div>
@@ -35,13 +28,47 @@ export default {
     points: {
       type: Array,
       default: () => []
-    }
+    },
+
   },
   data() {
     return {
-      center: null,
-      zoom: 13,
-      rotation: 0
+      center: [2159833.468576233, 6786155.512946144],
+      zoom: 7,
+      rotation: 0,
+      geo: [{
+        type: 'Feature',
+        id: 1,
+        geometry: {
+          type: 'Point',
+          coordinates: [2159833.468576233, 6786155.512946144],
+        },
+      },
+      {
+        type: 'Feature',
+        id: 2,
+        geometry: {
+          type: 'Point',
+          coordinates: [2159833.468576233, 6586155.512946144],
+        },
+      },
+      {
+        type: 'Feature',
+        id: 3,
+        geometry: {
+          type: 'Point',
+          coordinates: [2159833.468576233, 6586155.512946144],
+        },
+      },
+      {
+        type: 'Feature',
+        id: 3,
+        geometry: {
+          type: 'Point',
+          coordinates: [2159833.468576233, 6586155.512946144],
+        },
+      }
+      ]
     }
   }
 }
